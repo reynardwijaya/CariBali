a1846ddbbee995b6c8d5a8e2245d768eba17ef76
reynardwijaya <reynardwijaya710@gmail.com>
2026-09-12 15:59:23 +0700

    Remove unused Jetstream boilerplate and legacy views

--- resources/views/home/tempCodeRunnerFile.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <img 
-        src="{{ asset('postimage/' . $post->image) }}" 
-        alt="Post Image"
-        class="services_img"
-        style="padding: 20px; height:400px; width: auto; margin: auto; display: block;">
-</div>
